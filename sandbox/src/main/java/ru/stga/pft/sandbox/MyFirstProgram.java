e29780de2eb8df1650d5d9d4243317deccd30300
Boris Petrov <boris2p@mail.ru>
2023-07-10 12:12:31 +0300

    Добавлены  классы Rectangle, Square

--- sandbox/src/main/java/ru/stga/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stga/pft/sandbox/MyFirstProgram.java
@@ -2,9 +2,25 @@ package ru.stga.pft.sandbox;
 
 public class MyFirstProgram {
 
- public static void main(String[] arg) {
+    public static void main(String[] arg) {
 
-	System.out.println("Hello, world!");
-	}
+        hello("user");
+        hello("world");
+        hello("Alex");
+
+        Square s = new Square(5);
+
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area()) ;
+
+        Rectangle r = new Rectangle(5, 6);
+
+        System.out.println("Площадь прямоугольника со сторонами " + r.a +  " и " + r.b + " = " + r.area()) ;
+
+    }
+
+    public static void hello(String somebody) {
+
+        System.out.println("Hello, " + somebody + "!");
+    }
 
 }
